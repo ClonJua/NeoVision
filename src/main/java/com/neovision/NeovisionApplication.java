2aaa8eddedf1c7e8aec421850e3ac74e45c6cb97
Jose <jodanape89@hotmail.com>
2026-03-31 23:53:59 -0500

    git init test

---
+++ src/main/java/com/neovision/NeovisionApplication.java
@@ -0,0 +1,13 @@
+package com.neovision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeovisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeovisionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neovision/NeovisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neovision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeovisionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
